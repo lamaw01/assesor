307f63beb4eafed2dd4ee345367b932811e79f98
lamaw01 <janreylamaw@gmail.com>
2020-02-07 11:55:35 +0800

    ext

--- application/models/Excel_import_model.php
+++ application/models/Excel_import_model.php
@@ -139,7 +139,8 @@ class Excel_import_model extends CI_Model
 
 	function count_report()
 	{
-		$this->db->select('SUBSTRING(sheet2.pin_new,17,3) AS pin_new, sheet1.old_pin AS old_pin, sheet1.owner AS owner, 
+		$this->db->select('SUBSTRING(sheet2.pin_new,17,3) AS pin_new, sheet1.ext AS ext,
+		sheet1.old_pin AS old_pin, sheet1.owner AS owner, 
 		sheet1.owner_address AS owner_address, sheet1.td AS td, sheet1.title AS title, 
 		sheet1.cad_lot AS cad_lot, sheet1.area1 AS area1, sheet1.area2 AS area2, sheet1.kind1 AS kind1, 
 		sheet1.kind2 AS kind2, sheet1.actual_use AS actual_use');
@@ -153,13 +154,14 @@ class Excel_import_model extends CI_Model
 	function fetch_report($limit, $start)
 	{
 		$output = '';
-		$this->db->select('SUBSTRING(sheet2.pin_new,17,3) AS pin_new, sheet1.old_pin AS old_pin, sheet1.owner AS owner, 
+		$this->db->select('SUBSTRING(sheet1.old_pin,15,3) AS old_pin, sheet1.ext AS ext,
+		SUBSTRING(sheet2.pin_new,17,3) AS pin_new, sheet1.owner AS owner, 
 		sheet1.owner_address AS owner_address, sheet1.td AS td, sheet1.title AS title, 
 		sheet1.cad_lot AS cad_lot, sheet1.area1 AS area1, sheet1.area2 AS area2, sheet1.kind1 AS kind1, 
 		sheet1.kind2 AS kind2, sheet1.actual_use AS actual_use');
 		$this->db->from('sheet1');
 		$this->db->join('sheet2','sheet1.old_pin = sheet2.old_pin');		
-		$this->db->order_by("pin_new", "ASC");
+		$this->db->order_by("old_pin", "ASC");
 		$this->db->group_by('sheet1_id');
 		
 		$this->db->limit($limit, $start);
@@ -167,8 +169,9 @@ class Excel_import_model extends CI_Model
 		$output .= '
 		<table class="table table-bordered">
 		<tr>
-			<th><p>Pin_New</p></th>
 			<th><p>Old_pin</p></th>
+			<th><p>Ext</p></th>
+			<th><p>Pin_New</p></th>	
 			<th><p>Owner</p></th>
 			<th><p>Owner_Address</p></th>
 			<th><p>Td</p></th>
@@ -185,8 +188,9 @@ class Excel_import_model extends CI_Model
 		{
 		$output .= '
 		<tr>
-			<td><p>'.$row->pin_new.'</p></td>
 			<td><p>'.$row->old_pin.'</p></td>
+			<td><p>'.$row->ext.'</p></td>
+			<td><p>'.$row->pin_new.'</p></td>
 			<td><p>'.$row->owner.'</p></td>
 			<td><p>'.$row->owner_address.'</p></td>
 			<td><p>'.$row->td.'</p></td>
